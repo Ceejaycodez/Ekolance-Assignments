09efd0a1c8dce7d1350adbc575c6b255f0498919
Gerald Nwanwko <geraldcjnwankwo@outlook.com>
2022-10-18 14:51:05 +0100

    Create EtherWalletA.sol
    Week 1 EtherWalletA Assignment

---
+++ EtherWalletA.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract EtherWallet {
+    address payable public owner;
+    address payable public user;
+   
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    // receive() external payable {}
+
+    function deposit() external payable {
+        
+    }
+
+    function withdraw(uint _amount) external {
+        require(msg.sender == owner , "caller is not owner");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function senderWithdrawal(uint _amount) external payable{
+        require(msg.sender == user , "caller is not user");
+        require(msg.value <= _amount, "user can't withdraw more than amount deposited");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+}
